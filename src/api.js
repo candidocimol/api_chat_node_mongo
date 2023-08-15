7a88e4bb6a6cc70c983845898422b5d4f35e291e
xinaiderr <141289990+xinaiderr@users.noreply.github.com>
2023-08-15 20:25:53 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -7,6 +7,9 @@ const salaController = require("./controllers/salaController");
 const usuarioController = require("./controllers/usuarioController");
 const app = express();
 app.use(cors());
+app.listen(3000, () => {
+  console.log('Servidor API iniciado na porta 3000');
+});
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
